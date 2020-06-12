803b1f6b3a9d810749983a3b831854fae1db6874
sergiobaquero <sergiobaquero86@gmail.com>
2020-06-12 16:26:57 +0200

    Actualizacion referencia directorio

--- src/model/training.py
+++ src/model/training.py
@@ -8,7 +8,7 @@ import pandas as pd
 import time
 from sklearn.metrics import accuracy_score
 from sklearn.model_selection import train_test_split
-sys.path.append("/src")
+sys.path.append("./src")
 import parameters
 
 #For training
