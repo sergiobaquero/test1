3740a24347d0d54d174864cd2302bbd2bc052f21
sergiobaquero <sergiobaquero86@gmail.com>
2020-06-12 16:44:11 +0200

    Actualizacion fichero train para añadir referencia a variable de fichero de parametros

--- src/model/training.py
+++ src/model/training.py
@@ -9,7 +9,7 @@ import time
 from sklearn.metrics import accuracy_score
 from sklearn.model_selection import train_test_split
 sys.path.append("./")
-import parameters
+import parameters as params
 
 #For training
 def train():
@@ -18,7 +18,7 @@ def train():
     Y = dataset[['I']]
     
     #train test split
-    X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size = 0.2, random_state = 101)
+    X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size = params.test_size, random_state = 101)
     
     from sklearn.svm import SVC
     model = SVC(kernel='linear')
