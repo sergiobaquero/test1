7278514fc5fd59d4d1471040a483ceecbb7bb12a
Sergio Baquero <sergiobaquero86@gmail.com>
2020-06-13 15:57:29 +0200

    Sustitucion tipo comillas

--- src/model/test.py
+++ src/model/test.py
@@ -44,7 +44,7 @@ def test():
     pre=p.predict(X)
     print("LA PRECISION CON LOS DATOS DE TESTTTTTTTTTT ES DE:")
     print (accuracy_score(Y,pre)) #Prints the accuracy of the model
-    file = open(“accu”,”w”) 
+    file = open("accuracy.txt","w") 
     file.write(accuracy_score(Y,pre)) 
     file.close() 
 
